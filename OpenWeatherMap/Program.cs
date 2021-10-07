43f46c84555879a06b4cc546f25f8b18f0b2f5a9
Cedric <mrlee.truecoder@gmail.com>
2021-10-07 13:05:13 -0500

    Completed API Exercise 2

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Enter your API key:");
+            var apiKey = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter your CITY name:");
+                var city = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine("");
+
+                Console.WriteLine("Would you like to choose a different city?");
+                var choice = Console.ReadLine();
+                Console.WriteLine("");
+                if (choice.ToLower() == "no")
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
